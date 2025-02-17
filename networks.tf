dfd3bfb14affb2b049d11e78986321e4309d87c5
borbul2007 <78852894+borbul2007@users.noreply.github.com>
2025-02-17 18:37:29 +0300

    Update

--- networks.tf
+++ networks.tf
@@ -8,13 +8,13 @@ resource "yandex_vpc_network" "private" {
 
 # VPC subnets
 resource "yandex_vpc_subnet" "public" {
-  name           = "public-ru-central1-a"
+  name           = "public"
   zone           = var.default_zone
   network_id     = yandex_vpc_network.public.id
   v4_cidr_blocks = ["192.168.10.0/24"]
 }
 resource "yandex_vpc_subnet" "private" {
-  name           = "private-ru-central1-a"
+  name           = "private"
   zone           = var.default_zone
   network_id     = yandex_vpc_network.private.id
   v4_cidr_blocks = ["192.168.20.0/24"]
