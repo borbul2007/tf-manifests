56d371a3abea3ab96af9470e7132d2d77d260531
borbul2007 <78852894+borbul2007@users.noreply.github.com>
2025-02-17 17:34:55 +0300

    Update

--- networks.tf
+++ networks.tf
@@ -7,18 +7,18 @@ resource "yandex_vpc_network" "private" {
 }
 
 # VPC subnets
-resource "yandex_vpc_subnet" "public_a" {
-  name           = "public-ru-central1-a"
-  zone           = var.default_zone
-  network_id     = yandex_vpc_network.develop.id
-  v4_cidr_blocks = ["192.168.10.0/24"]
-}
-resource "yandex_vpc_subnet" "ptivate_a" {
-  name           = "public-ru-central1-a"
-  zone           = var.default_zone
-  network_id     = yandex_vpc_network.develop.id
-  v4_cidr_blocks = ["192.168.20.0/24"]
-}
+#resource "yandex_vpc_subnet" "public_a" {
+#  name           = "public-ru-central1-a"
+#  zone           = var.default_zone
+#  network_id     = yandex_vpc_network.develop.id
+#  v4_cidr_blocks = ["192.168.10.0/24"]
+#}
+#resource "yandex_vpc_subnet" "ptivate_a" {
+#  name           = "public-ru-central1-a"
+#  zone           = var.default_zone
+#  network_id     = yandex_vpc_network.develop.id
+#  v4_cidr_blocks = ["192.168.20.0/24"]
+#}
 
 # VPC NAT gateway
 
