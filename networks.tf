983a40cb382234496de24bdb471c505503c78120
borbul2007 <78852894+borbul2007@users.noreply.github.com>
2025-02-17 19:08:11 +0300

    Update

--- networks.tf
+++ networks.tf
@@ -20,20 +20,41 @@ resource "yandex_vpc_subnet" "private" {
   v4_cidr_blocks = ["192.168.20.0/24"]
 }
 
-# VPC NAT gateway
-resource "yandex_vpc_gateway" "nat-gateway" {
-  name = "nat-gateway"
-  shared_egress_gateway {}
-}
-
 # VPC route table
-resource "yandex_vpc_route_table" "route-table" {
-  name       = "route-table"
+resource "yandex_vpc_route_table" "nat-instance-route" {
+  name       = "nat-instance-route"
   network_id = yandex_vpc_network.public.id
   static_route {
     destination_prefix = "0.0.0.0/0"
-    gateway_id         = yandex_vpc_gateway.nat-gateway.id
+    next_hop_address   = yandex_compute_instance.nat-instance.network_interface.0.ip_address
   }
 }
 
 # VPC security group
+resource "yandex_vpc_security_group" "nat-instance-sg" {
+  name       = "nat-instance-sg"
+  network_id = yandex_vpc_network.public.id
+  egress {
+    protocol       = "ANY"
+    description    = "any"
+    v4_cidr_blocks = ["0.0.0.0/0"]
+  }
+  ingress {
+    protocol       = "TCP"
+    description    = "ssh"
+    v4_cidr_blocks = ["0.0.0.0/0"]
+    port           = 22
+  }
+  ingress {
+    protocol       = "TCP"
+    description    = "ext-http"
+    v4_cidr_blocks = ["0.0.0.0/0"]
+    port           = 80
+  }
+  ingress {
+    protocol       = "TCP"
+    description    = "ext-https"
+    v4_cidr_blocks = ["0.0.0.0/0"]
+    port           = 443
+  }
+}
--- vms.tf
+++ vms.tf
@@ -53,3 +53,29 @@ resource "yandex_compute_instance" "private" {
     ssh-keys = "ubuntu:${file("~/.ssh/id_rsa.pub")}"
   }
 }
+
+resource "yandex_compute_instance" "nat-instance" {
+  name        = local.vm_nat_name
+  platform_id = "standard-v1"
+  zone        = var.default_zone
+  resources {
+    core_fraction = 5
+    cores         = 2
+    memory        = 1
+  }
+  boot_disk {
+  name     = "boot-disk-nat"
+  type     = "network-hdd"
+  size     = "5"
+  image_id = "fd80mrhj8fl2oe87o4e1"
+  }
+  network_interface {
+    subnet_id          = yandex_vpc_subnet.public.id
+    security_group_ids = [yandex_vpc_security_group.nat-instance-sg.id]
+    nat                = true
+  }
+  scheduling_policy { preemptible = true }
+  metadata = {
+    user-data = "#cloud-config\nusers:\n  - name: "ubuntu"\n    groups: sudo\n    shell: /bin/bash\n    sudo: 'ALL=(ALL) NOPASSWD:ALL'\n    ssh_authorized_keys:\n      - ${file("~/.ssh/id_rsa.pub")}"
+  }
+}
