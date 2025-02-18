96a45ace7abc836eed8324692c228e3ed8a19cf8
borbul2007 <78852894+borbul2007@users.noreply.github.com>
2025-02-18 16:19:08 +0300

    Update

--- 15.2/instance-group.tf
+++ 15.2/instance-group.tf
@@ -1,3 +1,37 @@
-data "yandex_compute_instance_group" "instance_group" {
-  instance_group_id = "sfd827b91d99psvq5fjit"
+resource "yandex_compute_instance_group" "instance-group" {
+  name                = "instance-group"
+  folder_id           = var.folder_id
+  instance_template {
+    platform_id = var.vm_yandex_compute_instance_platform_id
+    resources {
+      cores         = var.vm_yandex_compute_instance_resources_cores
+      memory        = var.vm_yandex_compute_instance_resources_memory
+      core_fraction = var.vm_yandex_compute_instance_resources_core_fraction
+    }
+    boot_disk {
+      initialize_params {
+        image_id = "fd827b91d99psvq5fjit"
+      }
+    }
+    network_interface {
+      network_id         = var.yandex_vpc_network.network.id
+      subnet_ids         = [var.yandex_vpc_subnet.private.id]
+    }
+    scheduling_policy { preemptible = true }
+    metadata = {
+      user-data = "${file("cloud-init.yaml")}"
+    }
+  }
+  scale_policy {
+    fixed_scale {
+      size = 3
+    }
+  }
+  allocation_policy {
+    zones = [var.default_zone]
+  }
+  deploy_policy {
+    max_unavailable = 1
+    max_expansion   = 0
+  }
 }
---
+++ 15.2/networks.tf
@@ -0,0 +1,13 @@
+# VPC networks
+resource "yandex_vpc_network" "network" {
+  name = "network"
+}
+
+# VPC subnets
+resource "yandex_vpc_subnet" "private" {
+  name           = "private"
+  zone           = var.default_zone
+  network_id     = yandex_vpc_network.network.id
+  v4_cidr_blocks = var.private_yandex_vpc_subnet_default_cidr
+  route_table_id = yandex_vpc_route_table.nat.id
+}
