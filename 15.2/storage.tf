d027e3ccfe57eed156b3b83fe566b444104dd217
borbul2007 <78852894+borbul2007@users.noreply.github.com>
2025-02-18 15:23:29 +0300

    Update

--- 15.2/storage.tf
+++ 15.2/storage.tf
@@ -2,7 +2,7 @@ resource "yandex_storage_bucket" "bucket" {
   bucket = "boris-18-02-2025"
   anonymous_access_flags {
     read        = true
-    list        = true
-    config_read = true
+    list        = false
+    config_read = false
   }
 }
