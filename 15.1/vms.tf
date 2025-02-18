d51677808c74275aba26779b6774f3cc58b82ac2
borbul2007 <78852894+borbul2007@users.noreply.github.com>
2025-02-18 11:12:36 +0300

    Update

---
+++ 15.1/vms.tf
@@ -0,0 +1,80 @@
+resource "yandex_compute_instance" "public" {
+  name        = "public"
+  platform_id = "standard-v1"
+  zone        = var.default_zone
+  resources {
+    cores  = 2
+    memory = 1
+    core_fraction = 5
+  }
+  boot_disk {
+    initialize_params {
+      image_id = "fd8guaqmm9vfb6ev4cc8"
+      type     = "network-hdd"
+      size     = 10
+    }
+  }
+  network_interface {
+    subnet_id = yandex_vpc_subnet.public.id
+    nat       = true
+  }
+  scheduling_policy { preemptible = true }
+  metadata = {
+    serial-port-enable = 1
+    ssh-keys = "ubuntu:${file("~/nt-ssh/id_ed25519.pub")}"
+  }
+}
+
+resource "yandex_compute_instance" "private" {
+  name        = "private"
+  platform_id = "standard-v1"
+  zone        = var.default_zone
+  resources {
+    cores  = 2
+    memory = 1
+    core_fraction = 5
+  }
+  boot_disk {
+    initialize_params {
+      image_id = "fd8guaqmm9vfb6ev4cc8"
+      type     = "network-hdd"
+      size     = 10
+    }
+  }
+  network_interface {
+    subnet_id          = yandex_vpc_subnet.private.id
+  }
+  scheduling_policy { preemptible = true }
+  metadata = {
+    serial-port-enable = 1
+    ssh-keys = "ubuntu:${file("~/nt-ssh/id_ed25519.pub")}"
+  }
+}
+
+resource "yandex_compute_instance" "nat" {
+  name        = "nat"
+  platform_id = "standard-v1"
+  zone        = var.default_zone
+  resources {
+    core_fraction = 5
+    cores         = 2
+    memory        = 1
+  }
+  boot_disk {
+    initialize_params {
+      image_id = "fd80mrhj8fl2oe87o4e1"
+      type     = "network-hdd"
+      size     = 10
+    }
+  }
+  network_interface {
+    subnet_id          = yandex_vpc_subnet.public.id
+    ip_address         = "192.168.10.254"
+    nat                = true
+  }
+  scheduling_policy { preemptible = true }
+  metadata = {
+    serial-port-enable = 1
+    user-data          = "#cloud-config\nusers:\n  - name: ubuntu\n    groups: sudo\n    shell: /bin/bash\n    sudo: 'ALL=(ALL) NOPASSWD:ALL'\n    ssh_authorized_keys:\n      - ${file("~/nt-ssh/id_ed25519.pub")}"
+  }
+}
---
+++ 15.2/variables.tf
@@ -0,0 +1,14 @@
+variable "cloud_id" {
+  type        = string
+  default     = "b1guau0af4j7qkg1484e"
+}
+
+variable "folder_id" {
+  type        = string
+  default     = "b1gkg5og1lpl8fc1563m"
+}
+
+variable "default_zone" {
+  type        = string
+  default     = "ru-central1-a"
+}
