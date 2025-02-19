058d2ff8f5158d81d9b41900b42803213e94092e
borbul2007 <78852894+borbul2007@users.noreply.github.com>
2025-02-19 08:53:29 +0300

    Update

--- 15.2/instance-group.tf
+++ 15.2/instance-group.tf
@@ -10,13 +10,13 @@ resource "yandex_resourcemanager_folder_iam_member" "compute_editor" {
   folder_id  = var.folder_id
   role       = "compute.editor"
   member     = "serviceAccount:${yandex_iam_service_account.instance-group-sa.id}"
-#  depends_on = [yandex_iam_service_account.instance-group-sa]
+  depends_on = [yandex_iam_service_account.instance-group-sa]
 }
 resource "yandex_resourcemanager_folder_iam_member" "load_balancer_editor" {
   folder_id = var.folder_id
   role      = "load-balancer.editor"
   member    = "serviceAccount:${yandex_iam_service_account.instance-group-sa.id}"
-#  depends_on = [yandex_iam_service_account.instance-group-sa]
+  depends_on = [yandex_iam_service_account.instance-group-sa]
 }
 
 resource "yandex_compute_instance_group" "instance-group" {
