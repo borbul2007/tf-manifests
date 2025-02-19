ae49dd849be940ce7fe885eb5667122f220a7ff1
borbul2007 <78852894+borbul2007@users.noreply.github.com>
2025-02-19 09:04:05 +0300

    Update

--- 15.2/instance-group.tf
+++ 15.2/instance-group.tf
@@ -23,7 +23,7 @@ resource "yandex_compute_instance_group" "instance-group" {
   name                = "instance-group"
   folder_id           = var.folder_id
   service_account_id  = "${yandex_iam_service_account.instance-group-sa.id}"
-  depends_on          = [yandex_resourcemanager_folder_iam_member.compute_editor,yandex_resourcemanager_folder_iam_member.load_balancer_editor]
+  #depends_on          = [yandex_resourcemanager_folder_iam_member.compute_editor,yandex_resourcemanager_folder_iam_member.load_balancer_editor]
   instance_template {
     platform_id = var.vm_yandex_compute_instance_platform_id
     resources {
