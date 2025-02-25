995436693e197dd80a9d091583c73a5cbc7a5cbc
borbul2007 <78852894+borbul2007@users.noreply.github.com>
2025-02-25 09:51:52 +0300

    Update

--- 15.3/kms.tf
+++ 15.3/kms.tf
@@ -8,19 +8,25 @@ resource "yandex_resourcemanager_folder_iam_member" "storage-admin" {
   member    = "serviceAccount:${yandex_iam_service_account.storage-admin.id}"
 }
 
-resource "yandex_iam_service_account_static_access_key" "sa-static-key" {
+resource "yandex_iam_service_account_static_access_key" "key" {
   service_account_id = yandex_iam_service_account.storage-admin.id
   description        = "static access key for object storage"
 }
 
+resource "yandex_kms_symmetric_key" "key" {
+  name              = "key"
+  default_algorithm = "AES_128"
+  rotation_period   = "8760h"
+}
+
 resource "yandex_storage_bucket" "test" {
   bucket     = var.bucket_name
-  access_key = yandex_iam_service_account_static_access_key.sa-static-key.access_key
-  secret_key = yandex_iam_service_account_static_access_key.sa-static-key.secret_key
+  access_key = yandex_iam_service_account_static_access_key.key.access_key
+  secret_key = yandex_iam_service_account_static_access_key.key.secret_key
   server_side_encryption_configuration {
     rule {
       apply_server_side_encryption_by_default {
-        kms_master_key_id = yandex_kms_symmetric_key.key-a.id
+        kms_master_key_id = yandex_kms_symmetric_key.key.id
         sse_algorithm     = "aws:kms"
       }
     }
