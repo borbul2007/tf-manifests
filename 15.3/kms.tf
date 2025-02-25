2bb1e9e16aac8e695bab092977b297c0c033fe2a
borbul2007 <78852894+borbul2007@users.noreply.github.com>
2025-02-25 09:54:47 +0300

    Update

--- 15.3/kms.tf
+++ 15.3/kms.tf
@@ -18,17 +18,3 @@ resource "yandex_kms_symmetric_key" "key" {
   default_algorithm = "AES_128"
   rotation_period   = "8760h"
 }
-
-resource "yandex_storage_bucket" "bucket" {
-  bucket     = var.bucket_name
-  access_key = yandex_iam_service_account_static_access_key.key.access_key
-  secret_key = yandex_iam_service_account_static_access_key.key.secret_key
-  server_side_encryption_configuration {
-    rule {
-      apply_server_side_encryption_by_default {
-        kms_master_key_id = yandex_kms_symmetric_key.key.id
-        sse_algorithm     = "aws:kms"
-      }
-    }
-  }
-}
